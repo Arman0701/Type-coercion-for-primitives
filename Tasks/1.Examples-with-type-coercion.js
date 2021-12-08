8478dd8ee79da8bee1cf071e04ee989161fab6a5
Arman773 <tadevosyan889@gmail.com>
2021-12-08 22:58:22 +0400

    added exmples

---
+++ Tasks/1.Examples-with-type-coercion.js
@@ -0,0 +1,14 @@
+console.log("" + 1 + 0);                 // "10" type string
+console.log("" - 1 + 0);                 // -1 type number
+console.log(true + false);               // 1 type number
+console.log(6 / "3");                    // 2 type number
+console.log("2" * "3");                  // 6 type number
+console.log(4 + 5 + "px");               // "9px" type string
+console.log("$" + 4 + 5);                // "$45" type string
+console.log("4" - 2);                    // 2 type number
+console.log("4px" - 2);                  // NaN type number
+console.log(7 / 0);                      // Infinity type number
+console.log(" -9 " + 5);                 // "-95" type string
+console.log(" -9 " - 5);                 // -14 type number
+console.log(null + 1);                   // 1 type number
+console.log(undefined + 1);              // NaN type number
